36c56f8e22e1f1b5abda625268c2de948ec29c6f
andrew-wallace-594 <andrew.wallace@kellogg.northwestern.edu>
2022-04-20 18:26:31 +0000

    class on 4/15

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,44 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS sales_people;
+DROP TABLE IF EXISTS companies; 
+DROP TABLE IF EXISTS interactions; 
+
 -- CREATE TABLES
+CREATE TABLE contacts (
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+first_name TEXT,
+last_name TEXT, 
+email TEXT, 
+address TEXT, 
+phone_number TEXT, 
+company_id INTEGER, 
+role TEXT
+);
+
+CREATE TABLE sales_people ( 
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+first_name TEXT, 
+last_name TEXT, 
+email TEXT, 
+address TEXT
+);
+
+CREATE TABLE companies (
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+name TEXT
+);
+
+-- interactions is a "relationship" between sales people and contacts. This means this is a "join table"
+
+CREATE TABLE interactions (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+contact_id INTEGER,
+sales_team_id INTEGER,
+time text, 
+date TEXT, 
+type TEXT,
+notes TEXT
+);
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,45 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS followers;
+DROP TABLE IF EXISTS feed;
+
+CREATE TABlE users (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+first_name TEXT, 
+last_name TEXT, 
+username TEXT, 
+location TEXT
+);
+
+CREATE TABlE posts (
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+user_id INTEGER, 
+file_name TEXT,
+created_at TEXT
+); 
+
+-- the likes table serves as a join model between user and posts
+
+CREATE TABlE likes (
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+user_id INTEGER, 
+post_id INTEGER
+);
+
+CREATE TABLE comments (
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+user_id INTEGER,
+post_id INTEGER,
+comment TEXT
+);
+
+-- this is another join model between users and users
+
+CREATE TABLE followers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    follower_user_id INTEGER,
+    following_user_id INTEGER
+);
 
